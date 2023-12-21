b7df23486c1a3dda1ca6966b73802330f73200a9
yours-kaett <kentanthony2022@gmail.com>
2023-12-21 17:18:57 +0800

    Updated NutriSmart

--- nutrismart.sql
+++ nutrismart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 14, 2023 at 06:05 AM
+-- Generation Time: Dec 21, 2023 at 09:18 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_blood_pressures`
+--
+
+DROP TABLE IF EXISTS `tbl_blood_pressures`;
+CREATE TABLE IF NOT EXISTS `tbl_blood_pressures` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bp_value` varchar(50) NOT NULL,
+  `patient_id` int NOT NULL,
+  `created_at` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_blood_pressures`
+--
+
+INSERT INTO `tbl_blood_pressures` (`id`, `bp_value`, `patient_id`, `created_at`) VALUES
+(1, '120/10', 3, '2023-12-21');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_dietary_logging`
 --
 
@@ -31,28 +53,28 @@ DROP TABLE IF EXISTS `tbl_dietary_logging`;
 CREATE TABLE IF NOT EXISTS `tbl_dietary_logging` (
   `id` int NOT NULL AUTO_INCREMENT,
   `meal_id` int NOT NULL,
-  `time` varchar(50) NOT NULL,
-  `rice` varchar(50) NOT NULL,
-  `viand` varchar(50) NOT NULL,
-  `carbohydrates` varchar(50) NOT NULL,
-  `protein` varchar(50) NOT NULL,
-  `fat` varchar(50) NOT NULL,
-  `fiber` varchar(50) NOT NULL,
+  `time` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `rice` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `viand` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ingredients` text NOT NULL,
+  `carbohydrates` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `protein` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `fat` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `fiber` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `total_grams` varchar(50) NOT NULL,
-  `date` varchar(50) NOT NULL,
+  `date` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `patient_id` int NOT NULL,
   `blood_sugar_level` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbl_dietary_logging`
 --
 
-INSERT INTO `tbl_dietary_logging` (`id`, `meal_id`, `time`, `rice`, `viand`, `carbohydrates`, `protein`, `fat`, `fiber`, `total_grams`, `date`, `patient_id`, `blood_sugar_level`) VALUES
-(3, 1, '7:00 am', '-', 'Oatmeal Champorado with Fresh Fruits', '40', '7', '4', '6', '57', '2023-12-14', 3, '100'),
-(4, 2, '12:00 pm', 'Plain Rice', 'Grilled Bangus Salad', '15', '20', '10', '5', '50', '2023-12-14', 3, '100'),
-(5, 3, '8:00 pm', 'Plain Rice', 'Tinolang Manok', '30', '20', '5', '3', '58', '2023-12-14', 3, '100');
+INSERT INTO `tbl_dietary_logging` (`id`, `meal_id`, `time`, `rice`, `viand`, `ingredients`, `carbohydrates`, `protein`, `fat`, `fiber`, `total_grams`, `date`, `patient_id`, `blood_sugar_level`) VALUES
+(10, 1, '7:00 am', '-', 'Oatmeal Champorado with Fresh Fruits', '1/2 cup rolled oats, 1 tablespoon cocoa powder, 1-2 tablespoons coconut sugar or sweetener of choice, Fresh fruits (banana slices, cherry).', '40', '7', '4', '6', '57', '2023-12-22', 3, '100'),
+(11, 3, '8:00 pm', 'Plain Rice', 'Tinolang Manok', '150g chicken breast, Ginger, garlic, and onions, Green papaya slices and malunggay leaves, 1 cup cooked white rice.', '30', '20', '5', '3', '58', '2023-12-21', 3, '');
 
 -- --------------------------------------------------------
 
@@ -64,14 +86,14 @@ DROP TABLE IF EXISTS `tbl_dietary_meal`;
 CREATE TABLE IF NOT EXISTS `tbl_dietary_meal` (
   `id` int NOT NULL AUTO_INCREMENT,
   `meal_id` int NOT NULL,
-  `time` varchar(50) NOT NULL,
-  `rice` varchar(50) NOT NULL,
-  `viand` varchar(50) NOT NULL,
-  `ingredients` varchar(255) NOT NULL,
-  `carbohydrates` varchar(50) NOT NULL,
-  `protein` varchar(50) NOT NULL,
-  `fat` varchar(50) NOT NULL,
-  `fiber` varchar(50) NOT NULL,
+  `time` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `rice` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `viand` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ingredients` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `carbohydrates` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `protein` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `fat` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `fiber` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `hp_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `meal_id` (`meal_id`)
@@ -93,6 +115,48 @@ INSERT INTO `tbl_dietary_meal` (`id`, `meal_id`, `time`, `rice`, `viand`, `ingre
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_glucose_levels`
+--
+
+DROP TABLE IF EXISTS `tbl_glucose_levels`;
+CREATE TABLE IF NOT EXISTS `tbl_glucose_levels` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `glucose_value` varchar(50) NOT NULL,
+  `patient_id` int NOT NULL,
+  `created_at` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_glucose_levels`
+--
+
+INSERT INTO `tbl_glucose_levels` (`id`, `glucose_value`, `patient_id`, `created_at`) VALUES
+(1, '80', 3, '2023-12-22'),
+(2, '95', 3, '2023-12-23'),
+(3, '85', 3, '2023-12-24'),
+(4, '115', 3, '2023-12-25'),
+(5, '80', 3, '2023-12-26'),
+(6, '95', 3, '2023-12-27'),
+(7, '85', 3, '2023-12-28'),
+(8, '115', 3, '2023-12-29'),
+(9, '115', 3, '2024-01-03'),
+(10, '95', 3, '2024-01-05'),
+(11, '80', 3, '2024-01-06'),
+(12, '95', 3, '2024-01-07'),
+(13, '85', 3, '2024-01-08'),
+(14, '115', 3, '2024-01-010'),
+(15, '80', 3, '2024-01-13'),
+(16, '95', 3, '2024-01-14'),
+(17, '85', 3, '2024-01-16'),
+(18, '115', 3, '2024-01-17'),
+(19, '115', 3, '2024-01-18'),
+(20, '80', 3, '2024-01-20'),
+(21, '90', 3, '2024-01-28');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_goals`
 --
 
@@ -100,19 +164,20 @@ DROP TABLE IF EXISTS `tbl_goals`;
 CREATE TABLE IF NOT EXISTS `tbl_goals` (
   `id` int NOT NULL AUTO_INCREMENT,
   `date` varchar(50) NOT NULL,
+  `time` varchar(50) NOT NULL,
   `title` varchar(50) NOT NULL,
   `description` text NOT NULL,
   `patient_id` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  KEY `seeker_id` (`patient_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbl_goals`
 --
 
-INSERT INTO `tbl_goals` (`id`, `date`, `title`, `description`, `patient_id`) VALUES
-(1, '2023-12-17', 'test', 'qwertyuiop[', 3),
-(2, '2023-12-18', 'asdfg', 'test', 3);
+INSERT INTO `tbl_goals` (`id`, `date`, `time`, `title`, `description`, `patient_id`) VALUES
+(9, '2023-12-21', '05:12:46', 'test', 'testtttt', 3);
 
 -- --------------------------------------------------------
 
@@ -145,9 +210,9 @@ INSERT INTO `tbl_health_provider` (`id`, `email`, `username`, `password`) VALUES
 DROP TABLE IF EXISTS `tbl_meal`;
 CREATE TABLE IF NOT EXISTS `tbl_meal` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `meal` varchar(50) NOT NULL,
+  `meal` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_meal`
@@ -167,9 +232,9 @@ INSERT INTO `tbl_meal` (`id`, `meal`) VALUES
 DROP TABLE IF EXISTS `tbl_patient`;
 CREATE TABLE IF NOT EXISTS `tbl_patient` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
